73c0375f1c99f4f33f3d235439386a95fc76ed41
csb@kde-neon <claudimards@gmail.com>
2022-05-11 22:01:29 -0300

    Testes do repositório ok

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -2,16 +2,20 @@ import { v4 as uuidV4 } from "uuid";
 
 class User {
   // Complete aqui
-  id: string;
+  id?: string;
   name: string;
-  admin: boolean;
+  admin?: boolean;
   email: string;
-  created_at: Date;
-  updated_at: Date;
+  created_at?: Date;
+  updated_at?: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
+    }
+
+    if (!this.created_at) {
+      this.created_at = new Date();
     }
 
     this.admin = false;
